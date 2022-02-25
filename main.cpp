7c490b04532a83a51d85932f94efe17bf7515726
Adrian Toro <adriandesantis26@gmail.com>
2022-02-25 19:51:46 -0400

    Defines and Queue structure definitions
    Signed-off-by: Adrian Toro <adriandesantis26@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
 
+/**
+ * Defines and typedefs
+*/
+typedef struct node node_t;
+typedef struct queue queue_t;
+
+#define NUMBERS_QTY 2
+
+/**
+ * Node structure
+*/
+struct node{
+    int number;
+    struct node *next;
+    struct node *prev;
+};
+
+/**
+ * Queue structure definition
+*/
+struct queue {
+    node_t *head;
+    node_t *tail;
+};
+
 int main(){
 
     return 0;
