b48a4a461b0ed0c666eb7a6ca30ca3f6231a21fb
Adrian Toro <adriandesantis26@gmail.com>
2022-02-25 20:21:06 -0400

    Arithmetic operations added
    Signed-off-by: Adrian Toro <adriandesantis26@gmail.com>

--- main.cpp
+++ main.cpp
@@ -3,7 +3,51 @@
 
 #define NUMBERS_QTY 2
 
+/**
+ * Sums all numbers in the queue
+*/
+int sum(queue_t &q){
+    node_t *node = q.tail;
 
+    int sum = 0;
+    for(int i = 0; i < NUMBERS_QTY; i++){
+        sum += node->number;
+        node = node->prev;
+    }
+    return sum;
+}
+/**
+ * Substract last two numbers in the queue
+*/
+int substraction(queue_t &q){
+    node_t *node = q.tail;
+    return (node->prev->number - node->number);
+}
+/**
+ * Calculates the product of all numbers in the queue
+*/
+int product(queue_t &q){
+    node_t *node = q.tail;
+
+    int product = 0;
+    for(int i = 0; i < NUMBERS_QTY; i++){
+         product *= node->number;
+        node = node->prev;
+    }
+    return product;
+}
+/**
+ * Divides last two numbers in the queue
+*/
+int division(queue_t &q){
+    node_t *node = q.tail;
+
+    return (node->prev->number / node->number);
+}
+
+/**
+ * Application entrypoint
+*/
 int main(){
 
     return 0;
