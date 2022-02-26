2171fba21a9a8fece6e612bae436161e0f5dcf37
Adrian Toro <adriandesantis26@gmail.com>
2022-02-25 20:01:50 -0400

    Basic Queue operations added
    Signed-off-by: Adrian Toro <adriandesantis26@gmail.com>

--- main.cpp
+++ main.cpp
@@ -25,6 +25,78 @@ struct queue {
     node_t *tail;
 };
 
+/**
+ * Checks if given queue is empty.
+ * @param q: Queue reference.
+ * @return: True if empty, false otherwise.
+*/
+bool isQueueEmpty(queue_t &q){
+    return q.head == NULL;
+}
+/**
+ * Adds element to given queue.
+ * @param q: Queue reference.
+ * @param value: Value to be added to the queue.
+*/
+void addToQueue(queue_t &q, int value){
+    node_t *const node = new(node_t);
+    node->number = value;
+    node->next = NULL;
+    node->prev = NULL;
+
+    if(isQueueEmpty(q))
+        q.head = node;
+    else
+        q.tail->next = node;
+
+    node_t *prev = q.tail;
+    q.tail = node;
+    q.tail->prev = prev;
+
+}
+
+/**
+ * Removes element from given queue.
+ * @param q: Queue reference.
+ * @return: Value removed.
+*/
+int removeFromQueue(queue_t &q){
+    int value;
+    node_t *node;
+
+    node = q.head;
+    value = node->number;
+
+    q.head = q.head->next;
+    delete(node);
+
+    return value;
+}
+/**
+ * Displays elements in given queue.
+ * @param q: Queue reference.
+*/
+void showQueue(queue_t &q){
+    node_t *node;
+
+    node = q.head;
+
+    while(node != NULL){
+        printf("%i\n", node->number);
+        node = node->next;
+    }
+}
+/**
+ * Displays last element in given queue.
+ * @param q: Queue reference.
+*/
+void showLastInQueue(queue_t &q){
+    node_t *node;
+
+    node = q.tail;
+    printf("%i\n", node->number);
+}
+
 int main(){
 
     return 0;
